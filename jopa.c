4840c75f2ba7bb819fa8897aa7f49c573d74bdb6
Star Brilliant <m13253@hotmail.com>
2014-01-18 23:04:34 +0800

    Update source

--- jopa.c
+++ jopa.c
@@ -1,25 +1,78 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <jack/jack.h>
+#include <jack/ringbuffer.h>
 #include <pulse/simple.h>
 #include <pulse/error.h>
 
 #define CallOrNull(func, var) { if((var)) { (func)((var)); (var) = NULL; } }
 
+pthread_cond_t DataReadyPlay;
+pthread_mutex_t DataReadyPlayMutex;
 pa_simple *PulseAudio = NULL;
 pa_sample_spec PulseSample = {
     .format = PA_SAMPLE_FLOAT32,
     .rate = 48000,
     .channels = 2
 };
+jack_client_t *Jack = NULL;
+int JackPortNameSize = 0;
+int isJackQuitting = 0;
 
 void cleanup() {
     CallOrNull(pa_simple_free, PulseAudio);
+    CallOrNull(jack_client_close, Jack);
+}
+
+void JackOnError(const char *desc) {
+    fprintf(stderr, "JACK error: %s\n", desc);
+    isJackQuitting = 1;
+    pthread_cond_broadcast(&DataReadyPlay);
+}
+
+void JackOnShutdown(void *arg) {
+    fputs("JACK server is shutting down, quitting.\n", stderr);
+    isJackQuitting = 1;
+    pthread_cond_broadcast(&DataReadyPlay);
+}
+
+int JackOnBufferSize(jack_nframes_t nframes, void *arg) {
+    return 0;
+}
+
+void JackOnConnect(jack_port_id_t a, jack_port_id_t b, int connect, void *arg) {
+}
+
+int JackOnProcess(jack_nframes_t nframes, void *arg) {
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
     int ErrorCode;
 
+    ErrorCode = pthread_cond_init(&DataReadyPlay, NULL);
+    if(ErrorCode) {
+        fputs("Failed on pthread_cond_init.\n", stderr);
+        return 1;
+    }
+    ErrorCode = pthread_mutex_init(&DataReadyPlayMutex, NULL);
+    if(ErrorCode) {
+        fputs("Failed on pthread_mutex_init.\n", stderr);
+        return 1;
+    }
+    Jack = jack_client_open("JACK over PulseAudio", JackNoStartServer, NULL);
+    if(!Jack) {
+        fputs("Failed to connect to JACK. Run 'jackd -d dummy' first.\n", stderr);
+        cleanup();
+        return 1;
+    }
+    isJackQuitting = 0;
+    jack_set_error_function(JackOnError);
+    jack_on_shutdown(Jack, JackOnShutdown, NULL);
+    jack_set_buffer_size_callback(Jack, JackOnBufferSize, NULL);
+    jack_set_process_callback(Jack, JackOnProcess, NULL);
+    jack_set_port_connect_callback(Jack, JackOnConnect, NULL);
+    PulseSample.rate=jack_get_sample_rate(Jack);
     if(!(PulseAudio = pa_simple_new(
             /* server */ NULL, 
             /* name */ "Jack over PulseAudio",
@@ -31,7 +84,21 @@ int main(int argc, char *argv[]) {
             /* attr */ NULL,
             /* error */ &ErrorCode
     ))) {
-        fprintf(stderr, "Cannot open a stream for playback.\n");
+        fprintf(stderr, "Cannot open a stream for playback: %s\n", pa_strerror(ErrorCode));
+        cleanup();
+        return 1;
+    }
+    fprintf(stderr, "Sample rate is %luHz.\n", (unsigned long int) PulseSample.rate);
+    JackPortNameSize = jack_port_name_size();
+    ErrorCode = jack_activate(Jack);
+    if(ErrorCode) {
+        fputs("Failed to activate JACK client.\n", stderr);
+        cleanup();
+        return 1;
+    }
+    pthread_mutex_lock(&DataReadyPlayMutex);
+    for(;;) {
+        pthread_cond_wait(&DataReadyPlay, &DataReadyPlayMutex);
     }
     cleanup();
     return 0;
